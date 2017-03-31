0d9fb7bf1bcf0bb5bc95be12aac67f0c6fa5902f
John Gruber <gruberjl@gmail.com>
2017-03-31 16:27:29 -0400

    added theme support

--- functions.php
+++ functions.php
@@ -10,6 +10,9 @@ add_action( 'wp_enqueue_scripts', 'fabric_scripts' );
 
 add_theme_support( 'title-tag' );
 add_theme_support( 'post-thumbnails' );
+add_theme_support( 'automatic-feed-links' );
+add_theme_support( 'post-thumbnails' );
+set_post_thumbnail_size( 825, 510, true );
 
 function register_my_menus() {
   register_nav_menus(
