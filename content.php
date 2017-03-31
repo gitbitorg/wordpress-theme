3af250c2da5705e6faa6054d7327d4ec78014234
John Gruber <gruberjl@gmail.com>
2017-03-30 20:24:21 -0400

    added page

--- content.php
+++ content.php
@@ -1,7 +1,15 @@
-<div class="blog-post">
-	<h2 class="blog-post-title"><?php the_title(); ?></h2>
-	<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
+<div class='app-wrapper'>
+	<article class='ms-Grid'>
+		<div class='content'>
+			<header class='ms-Grid-row'>
+				<h1 class="article-title ms-font-su ms-Grid-col ms-u-sm6 ms-u-md8 ms-u-lg10"><?php the_title(); ?></h1>
+				<span class="ms-fontWeight-light ms-Grid-col ms-u-sm6 ms-u-md8 ms-u-lg10"><?php the_date(); ?></span>
+			</header>
 
- <?php the_content(); ?>
-
+			<section>
+				<?php the_content(); ?>
+			</section>
+			<div>class='ms-Grid-col ms-u-sm12'</div>
+		</div>
+	</article>
 </div>
---
+++ page.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<main>
+  <?php
+    if ( have_posts() ) : while ( have_posts() ) : the_post();
+
+      get_template_part( 'content', get_post_format() );
+
+    endwhile; endif;
+  ?>
+</main>
+
+		</div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
