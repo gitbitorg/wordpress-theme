58584a70fd897a71934cc77912c16030c72f781c
John Gruber <gruberjl@gmail.com>
2017-04-04 11:58:11 -0400

    updated background

--- index.php
+++ index.php
@@ -16,26 +16,26 @@
 ?>
 
 
-			<?php get_header(); ?>
-			<main>
-				<div class='app-wrapper'>
-					<div class='ms-Grid'>
-						<div class='ms-Grid-row'>
-							<?php
-								if ( have_posts() ) : while ( have_posts() ) : the_post();
-									get_template_part( 'content', get_post_format() );
-								endwhile; endif;
-							?>
-						</div>
-					</div>
-				</div>
-			</main>
-			<nav class='app-wrapper'>
-				<ul class="pager">
-					<li><?php next_posts_link( 'Previous' ); ?></li>
-					<li><?php previous_posts_link( 'Next' ); ?></li>
-				</ul>
-			</nav>
-			<?php /* get_sidebar(); */ ?>
+<?php get_header(); ?>
+<main class='grey-background'>
+	<div class='app-wrapper'>
+		<div class='ms-Grid'>
+			<div class='ms-Grid-row'>
+				<?php
+					if ( have_posts() ) : while ( have_posts() ) : the_post();
+						get_template_part( 'template-parts/card/content', get_post_format() );
+					endwhile; endif;
+				?>
+			</div>
+		</div>
+	</div>
+</main>
+<nav class='app-wrapper'>
+	<ul class="pager">
+		<li><?php next_posts_link( 'Previous' ); ?></li>
+		<li><?php previous_posts_link( 'Next' ); ?></li>
+	</ul>
+</nav>
+<?php /* get_sidebar(); */ ?>
 
 <?php get_footer(); ?>
