12e08ecf76c4de824936fd316cba4c014a53ed86
John Gruber <gruberjl@gmail.com>
2017-04-07 13:16:09 -0400

    added open graph to pages

--- page.php
+++ page.php
@@ -1,22 +1,21 @@
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
   <?php get_template_part( 'template-parts/head/site-head' ); ?>
+  <?php get_template_part( 'template-parts/head/og-page', get_post_format() ); ?>
 </head>
 <body id="body" <?php body_class(); ?>>
   <div class='site'>
     <?php get_header(); ?>
 
     <main>
-      <?php
-        if ( have_posts() ) : while ( have_posts() ) : the_post();
-          get_template_part( 'template-parts/post/meta' );
-          get_template_part( 'template-parts/post/content', get_post_format() );
-        endwhile; endif;
-      ?>
+      <?php get_template_part( 'template-parts/post/meta' ); ?>
+      <?php get_template_part( 'template-parts/post/content', get_post_format() ); ?>
     </main>
 
     <?php get_footer(); ?>
   </div>
 </body>
 </html>
+<?php endwhile; endif; ?>
---
+++ template-parts/head/og-page.php
@@ -0,0 +1,5 @@
+<meta property="og:url"           content='<?php the_permalink(); ?>' />
+<meta property="og:title"         content="<?php the_title(); ?>" />
+<meta property="og:description"   content="<?php echo(get_the_excerpt()); ?>" />
+<meta property="og:image"         content="<?php the_post_thumbnail_url(); ?>" />
+<meta property="og:type"          content="website" />
