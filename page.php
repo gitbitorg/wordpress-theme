a887a3ef0f83161622878d7cdae352f37b6adf80
John Gruber <gruberjl@gmail.com>
2017-04-07 23:21:28 -0400

    added sidebar to page

--- page.php
+++ page.php
@@ -3,15 +3,29 @@
 <html <?php language_attributes(); ?>>
 <head>
   <?php get_template_part( 'template-parts/head/site-head' ); ?>
+  <?php get_template_part( 'template-parts/post/meta' ); ?>
 </head>
 <body id="body" <?php body_class(); ?>>
   <div class='site'>
     <?php get_header(); ?>
 
-    <main>
-      <?php get_template_part( 'template-parts/post/meta' ); ?>
-      <?php get_template_part( 'template-parts/post/content', get_post_format() ); ?>
-    </main>
+    <div class='app-wrapper'>
+      <div class='ms-Grid'>
+        <div class='ms-Grid-row'>
+          <main class='ms-Grid-col ms-u-sm12 ms-u-xl9 <?php echo get_post_format() ?>'>
+            <?php get_template_part( 'template-parts/post/content', get_post_format() ); ?>
+          </main>
+          <nav class='sidebar ms-Grid-col ms-u-sm12 ms-u-xl3'>
+            This is the sidebar
+          </nav>
+        </div>
+        <div class='ms-Grid-row'>
+          <aside class='ms-Grid-col ms-u-sm12 ms-u-xl9 <?php echo get_post_format() ?>'>
+            <?php if ( comments_open() || get_comments_number() ) : comments_template(); endif; ?>
+          </aside>
+        </div>
+      </div>
+    </div>
 
     <?php get_footer(); ?>
   </div>
