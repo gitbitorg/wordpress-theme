7ddb00bcdeb838a66834593cb8c4e2dbef60a778
John Gruber <gruberjl@gmail.com>
2017-04-09 09:14:56 -0400

    removed formatting on aside

--- template-parts/post/content-aside.php
+++ template-parts/post/content-aside.php
@@ -1,9 +1,3 @@
-<article class='ms-Grid ms-font-xl <?php echo get_post_format() ?>'>
-	<div class='content'>
-		<div class='article-body ms-Grid-row'>
-			<section class="ms-fontWeight-light ms-Grid-col ms-u-sm12">
-				<?php the_content(); ?>
-			</section>
-		</div>
-	</div>
+<article class='ms-fontWeight-light ms-font-xl <?php echo get_post_format() ?>'>
+	<?php the_content(); ?>
 </article>
