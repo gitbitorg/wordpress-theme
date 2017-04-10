ad3ffbcba237e8fd264a270c631ecb23e4421221
John Gruber <gruberjl@gmail.com>
2017-04-10 15:09:40 -0400

    centered image

--- template-parts/post/content-status.php
+++ template-parts/post/content-status.php
@@ -3,7 +3,7 @@
 		<header class='ms-Grid-row'>
 			<h1 class="article-title ms-font-su ms-Grid-col ms-u-sm12 ms-u-md8 ms-u-lg10"><?php the_title(); ?></h1>
 			<span class="ms-fontWeight-light ms-Grid-col ms-u-sm12 ms-u-md8 ms-u-lg10"><?php the_date(); ?></span>
-			<div class="ms-fontWeight-light ms-Grid-col ms-u-sm12">
+			<div class="ms-fontWeight-light ms-Grid-col ms-u-sm12 ms-u-textAlignCenter">
 				<?php the_post_thumbnail('featured-image'); ?>
 			</div>
 		</header>
