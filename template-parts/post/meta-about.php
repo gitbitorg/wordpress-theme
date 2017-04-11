8d83a50a171fba7ebbe1a6e7a1b512a6f790348f
John Gruber <gruberjl@gmail.com>
2017-04-11 14:55:36 -0400

    added rich text formatting

--- template-parts/post/meta-about.php
+++ template-parts/post/meta-about.php
@@ -1,9 +1,43 @@
 <div class="about" style="display: none;">
-  <span itemprop="publisher" itemscope itemtype="http://schema.org/Organization">
-    <span itemprop="name" class="ms-font-xxl site-title">GitBit</span>
-  </span>
+  <span itemprop="name"><?php the_title(); ?></span>
+  <span itemprop="headline"><?php the_title(); ?></span>
+  <span itemprop="author"><?php echo the_author(); ?></span>
+  <a itemprop="mainEntityOfPage" href="<?php the_permalink(); ?>"><span itemprop="name"><?php the_title(); ?></span></a>
   <span itemprop="dateCreated" content="<?php the_date_xml(); ?>"><?php the_date('F j, Y'); ?></span>
-  <div class="author" itemprop="author" itemscope itemtype="http://schema.org/Person">
-    <span itemprop="name" content="<?php the_date_xml(); ?>"><?php the_author(); ?></span>
+  <span itemprop="datePublished" content="<?php the_date_xml(); ?>"><?php the_date('F j, Y'); ?></span>
+  <span itemprop="dateModified" content="<?php the_modified_date('Y-m-d'); ?>"><?php the_modified_date('Y-m-d'); ?></span>
+  <div itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
+    <img src="<?php the_post_thumbnail_url( 'large' ); ?>">
+    <meta itemprop="url" content="<?php the_post_thumbnail_url( 'large' ); ?>">
+    <meta itemprop="width" content="600">
+    <meta itemprop="height" content="600">
   </div>
+
+  <span itemprop="publisher" itemscope itemtype="http://schema.org/Organization">
+    <span itemprop="name">GitBit</span>
+    <span itemprop="url" content="http://gitbit.org/"></span>
+    <div itemprop="logo" itemscope itemtype="https://schema.org/ImageObject">
+      <img src="<?php bloginfo('template_directory'); ?>/assets/gitbit-logo-160x60.png">
+      <meta itemprop="url" content="<?php bloginfo('template_directory'); ?>/assets/gitbit-logo-160x60.png">
+      <meta itemprop="width" content="160">
+      <meta itemprop="height" content="60">
+    </div>
+  </span>
+  <?php
+    $categories = get_the_category();
+    if ($categories) {
+      foreach($categories as $category) { echo '<span itemprop="about">' . $category->name . '</about>'; }
+    }
+  ?>
+  <span itemprop="keywords"><?php
+    $posttags = get_the_tags();
+    if ($posttags) {
+      $count=0;
+      foreach($posttags as $tag) {
+        if ($count > 0) { echo ', '; }
+        echo $tag->name;
+        $count++;
+      }
+    }
+  ?></span>
 </div>
--- template-parts/post/meta-header.php
+++ template-parts/post/meta-header.php
@@ -1,7 +1,7 @@
 <header class='ms-Grid-row'>
-  <h1 itemprop="name" class="article-title ms-font-su ms-Grid-col ms-u-sm12 ms-u-md8 ms-u-lg10"><?php the_title(); ?></h1>
+  <h1 class="article-title ms-font-su ms-Grid-col ms-u-sm12 ms-u-md8 ms-u-lg10"><?php the_title(); ?></h1>
   <?php $date_published_style = is_page() ? 'style="display:none;"' : null ?>
-  <span <?php echo $date_published_style ?> itemprop="datePublished" content="<?php the_modified_date('Y-m-d'); ?>" class="ms-fontWeight-light ms-Grid-col ms-u-sm12 ms-u-md8 ms-u-lg10"><?php the_modified_date('F j, Y'); ?></span>
+  <span <?php echo $date_published_style ?> class="ms-fontWeight-light ms-Grid-col ms-u-sm12 ms-u-md8 ms-u-lg10"><?php the_modified_date('F j, Y'); ?></span>
   <div class="ms-fontWeight-light ms-Grid-col ms-u-sm12 ms-u-textAlignCenter">
     <?php the_post_thumbnail('featured-image'); ?>
   </div>
