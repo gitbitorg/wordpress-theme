99b2129e54f6110f0ece3b798f506684e58a1edd
john <gruberjl@gmail.com>
2017-04-14 16:13:16 -0400

    added services code

--- page-services-not-used.php
+++ page-services-not-used.php
@@ -18,48 +18,53 @@
             <p class="no-margin"><span class="color-teal price">$18</span><span>/month</span></p>
           </div>
         </header>
-        <section>
-          <p>feature 1</p>
-          <p>feature 2</p>
-          <p>feature 3</p>
+        <section class="package-features">
+          <p>24 hours a day, 365 days a year</p>
+          <p>phone & email support</p>
+          <p>PC & Mac</p>
+          <p>Phone & Tablet</p>
+          <p>Custom knowledge base</p>
         </section>
-        <section>
+        <section class="package-cta">
           <button class="package-button ms-Button ms-Button--primary"><label class="ms-fontSize-xl">Contact Sales</label></button>
         </section>
       </div>
       <div class="ms-Grid-col ms-u-sm12 ms-u-lg4 package ms-u-textAlignCenter ms-font-xl">
         <header class="package-container">
-          <h2 class="package-heading ms-font-xxl ms-fontWeight-semibold">24/7 Support</h2>
-          <p class="package-subheading color-grey ms-fontSize-l">Worry-free support from certified engineers</p>
+          <h2 class="package-heading ms-font-xxl ms-fontWeight-semibold">Business Essentials</h2>
+          <p class="package-subheading color-grey ms-fontSize-l">Keep up with heavy workloads while protecting client information</p>
           <div class="price-container">
             <p class="ms-font-s-plus color-grey no-margin">starting at</p>
-            <p class="no-margin"><span class="color-teal price">$18</span><span>/month</span></p>
+            <p class="no-margin"><span class="color-teal price">$34</span><span>/month</span></p>
           </div>
         </header>
-        <section>
-          <p>feature 1</p>
-          <p>feature 2</p>
-          <p>feature 3</p>
+        <section class="package-features">
+          <p>Secure email</p>
+          <p>Document management</p>
+          <p>Real time messaging</p>
+          <p>compliant with world-class industry standards</p>
+          <p>Store, access, and share documents securely from anywhere</p>
         </section>
-        <section>
+        <section class="package-cta">
           <button class="package-button ms-Button ms-Button--primary"><label class="ms-fontSize-xl">Contact Sales</label></button>
         </section>
       </div>
       <div class="ms-Grid-col ms-u-sm12 ms-u-lg4 package ms-u-textAlignCenter ms-font-xl">
         <header class="package-container">
-          <h2 class="package-heading ms-font-xxl ms-fontWeight-semibold">24/7 Support</h2>
-          <p class="package-subheading color-grey ms-fontSize-l">Worry-free support from certified engineers</p>
+          <h2 class="package-heading ms-font-xxl ms-fontWeight-semibold">Consulting</h2>
+          <p class="package-subheading color-grey ms-fontSize-l">Digital experts in the legal industry</p>
           <div class="price-container">
             <p class="ms-font-s-plus color-grey no-margin">starting at</p>
-            <p class="no-margin"><span class="color-teal price">$18</span><span>/month</span></p>
+            <p class="no-margin"><span class="color-teal price">Varies</span><span></span></p>
           </div>
         </header>
-        <section>
-          <p>feature 1</p>
-          <p>feature 2</p>
-          <p>feature 3</p>
+        <section class="package-features">
+          <p>IT engineers</p>
+          <p>Cloud architects</p>
+          <p>Business technology consultation</p>
+          <p>Digital marketing</p>
         </section>
-        <section>
+        <section class="package-cta">
           <button class="package-button ms-Button ms-Button--primary"><label class="ms-fontSize-xl">Contact Sales</label></button>
         </section>
       </div>
@@ -77,10 +82,11 @@
   padding-bottom: 200px;
 }
 #services-page .packages {
-  margin-top: -100px;
+  margin-top: -150px;
 }
 #services-page .package {
   background-color:white;
+  margin-top: 12px;
 }
 #services-page .package + .package {
   border-left: 1px solid rgb(216, 216, 216);
@@ -90,6 +96,7 @@
 }
 #services-page .package-subheading {
   margin-top:0px;
+  min-height: 45px;
 }
 #services-page .price-container {
   margin-top: 30px;
@@ -101,6 +108,13 @@
 }
 #services-page .package-container {
   border-bottom: 1px solid #eee;
+}
+#services-page .package-features {
+  min-height: 260px;
+}
+#services-page .package-cta {
+  margin-top: 30px;
+  margin-bottom: 30px;
 }
 #services-page .package-button {
   color: white;
