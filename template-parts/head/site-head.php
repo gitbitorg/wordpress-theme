55d846c0db50992752d4ca35303ab5734822b5a5
john <gruberjl@gmail.com>
2017-04-25 09:07:02 -0400

    Added pintrest domain validation

--- template-parts/head/site-head.php
+++ template-parts/head/site-head.php
@@ -1,6 +1,8 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name='viewport' content='width=device-width, initial-scale=1'/>
 
+<meta name="p:domain_verify" content="ac9cc8bb14dc4cd582f5cda5a532f4e0"/>
+
 <link rel='apple-touch-icon' sizes='57x57' href='<?php echo esc_url( get_template_directory_uri() ); ?>/assets/apple-icon-57x57.png'/>
 <link rel='apple-touch-icon' sizes='60x60' href='<?php echo esc_url( get_template_directory_uri() ); ?>/assets/apple-icon-60x60.png'/>
 <link rel='apple-touch-icon' sizes='72x72' href='<?php echo esc_url( get_template_directory_uri() ); ?>/assets/apple-icon-72x72.png'/>
