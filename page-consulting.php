19f82a855446429afc56637f81fbde7040c0420c
john <gruberjl@gmail.com>
2017-04-26 23:10:01 -0400

    Consulting

---
+++ page-consulting.php
@@ -0,0 +1,224 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+  <?php get_template_part( 'template-parts/head/site-head' ); ?>
+</head>
+<body id="body" <?php body_class(); ?>>
+  <div class='site'>
+    <?php get_header(); ?>
+
+    <main class='homepage'>
+      <section class="hero" style='background-image:url("<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/business-suit.jpg");'>
+        <div class='app-wrapper'>
+          <div class='ms-Grid'>
+            <div class='ms-Grid-row'>
+              <header class="ms-Grid-col ms-u-sm11 ms-u-lg8 ms-u-lgPush2 color-white" style="background: rgba(33, 33, 33, .95);">
+				<div class="ms-u-textAlignCenter">
+					<h1 class="ms-font-su">Consulting</h1>
+					<span class="subtitle ms-font-xxl">Grow. Optimize. Compete.</span>
+				</div>
+              </header>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div class='app-wrapper'>
+          <div class='ms-Grid'>
+            <div class='ms-Grid-row'>
+              <div class="ms-Grid-col ms-u-sm12 ms-u-md6 ms-u-lg4">
+                <div class="card ms-u-textAlignCenter" style="max-width:350px;">
+                  <div><i class="ms-Icon ms-Icon--Heart card-icon color-o365" aria-hidden="true"></i></div>
+                  <div class='card-contents'>
+                    <h2 class="ms-font-su card-title">Obtain expert insight</h2>
+                    <div class='excerpt ms-font-xl'>Work with professionals to assess your companies needs. Learn how to better utlize technology and services to provide additional value. Follow through with these processes with expert guidance along the way.</div>
+                  </div>
+                </div>
+              </div>
+              <div class="ms-Grid-col ms-u-sm12 ms-u-md6 ms-u-lg4">
+                <div class="card ms-u-textAlignCenter" style="max-width:350px;">
+                  <div><i class="ms-Icon ms-Icon--Chart card-icon color-green" aria-hidden="true"></i></div>
+                  <div class='card-contents'>
+                    <h2 class="ms-font-su card-title">Gain leverage over competitors</h2>
+                    <div class='excerpt ms-font-xl'>Discover new ways your business can gain an advatage over competitors. Improve best practices, implement innovative solutions, and achieve benchmarks more efficiently.</div>
+                  </div>
+                </div>
+              </div>
+              <div class="ms-Grid-col ms-u-sm12 ms-u-md6 ms-u-lg4">
+                <div class="card ms-u-textAlignCenter" style="max-width:350px;">
+                  <div><i class="ms-Icon ms-Icon--PieDouble card-icon color-blue" aria-hidden="true"></i></div>
+                  <div class='card-contents'>
+                    <h2 class="ms-font-su card-title">Remain focused</h2>
+                    <div class='excerpt ms-font-xl'>Tasks such as finding the right talent, staying up to date with technology, and keeping up on digital marketing can be extremely time consuming and distracting. Streamlining or outsourcing these processes will help you stay focused on your company goals.</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="hero grey-background">
+        <div class='app-wrapper'>
+          <div class='ms-Grid'>
+            <div class='ms-Grid-row'>
+				<div class="ms-Grid-col ms-u-sm12 ms-u-xl6">
+					<blockquote>
+						<img class="quotemark" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/open-quote.png" />
+						<h3 class="ms-font-xxl quote">Expert consulting helped us feel more prepared to <strong class="color-blue">meet our deliverables</strong>.</h3>
+					</blockquote>
+				</div>
+  				<div class="ms-Grid-col ms-u-sm12 ms-u-xl6">
+					<blockquote>
+						<img class="quotemark" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/open-quote.png" />
+						<h3 class="ms-font-xxl quote">The consultants were <strong class="color-blue">focused and flexible</strong> throughout the entire process.</h3>
+					</blockquote>
+				</div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="hero">
+        <div class='app-wrapper'>
+          <div class='ms-Grid'>
+            <div class='ms-Grid-row'>
+				<div class="ms-Grid-col ms-u-sm12 ms-u-xl6 ms-u-textAlignCenter">
+					<div class="ms-Grid-col ms-u-sm12 ms-u-textAlignCenter">
+						<img class="hero-image" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/cindy.jpg" alt="Women smiling" width="30%"/>
+					</div>
+					<div class="ms-Grid-col ms-u-sm12 ms-u-textAlignCenter">
+						<img class="hero-image" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/charles.jpg" alt="Model" width=30%" style="border-radius:50%" />
+						<img class="hero-image" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/dave.jpg" alt="Man on Microsoft Surface" width="30%"/>
+					</div>
+				</div>
+				<div class="ms-Grid-col ms-u-sm12 ms-u-xl6">
+					<div class='ms-Grid-row'>
+						<div class="ms-Grid-col ms-u-sm1 ms-u-textAlignRight">
+							<p class="ms-font-xxl color-green"><i class="ms-Icon ms-Icon--CheckMark" aria-hidden="true"></i></p>
+						</div>
+						<div class="ms-Grid-col ms-u-sm11">
+							<p class="ms-font-xl">End the cycle of resolving short-term needs. Develop an IT roadmap and act more inline with the strategy of your organization.</p>
+						</div>
+					</div>
+					<div class='ms-Grid-row'>
+						<div class="ms-Grid-col ms-u-sm1 ms-u-textAlignRight">
+							<p class="ms-font-xxl color-green"><i class="ms-Icon ms-Icon--CheckMark" aria-hidden="true"></i></p>
+						</div>
+						<div class="ms-Grid-col ms-u-sm11">
+							<p class="ms-font-xl">With growth in global competition, hiring the right candidate has become more competitive. Find the talent you need, when you need it.</p>
+						</div>
+					</div>
+					<div class='ms-Grid-row'>
+						<div class="ms-Grid-col ms-u-sm1 ms-u-textAlignRight">
+							<p class="ms-font-xxl color-green"><i class="ms-Icon ms-Icon--CheckMark" aria-hidden="true"></i></p>
+						</div>
+						<div class="ms-Grid-col ms-u-sm11">
+							<p class="ms-font-xl">Save money, time, and energy with Cloud Computing. Experience the future of IT Hosting.</p>
+						</div>
+					</div>
+					<div class='ms-Grid-row'>
+						<div class="ms-Grid-col ms-u-sm1 ms-u-textAlignRight">
+							<p class="ms-font-xxl color-green"><i class="ms-Icon ms-Icon--CheckMark" aria-hidden="true"></i></p>
+						</div>
+						<div class="ms-Grid-col ms-u-sm11">
+							<p class="ms-font-xl">Traditional marketing is no longer enough. Engage with customers and brand your business through digital mediums.</p>
+						</div>
+					</div>
+				</div>
+            </div>
+          </div>
+        </div>
+      </section>
+	  <section class="hero grey-background">
+        <div class='app-wrapper'>
+          <div class='ms-Grid'>
+			<div class="ms-Grid-row">
+				<div class="ms-Grid-col ms-u-sm12 ms-u-textAlignCenter">
+					<h2 class="ms-font-xxl">Consulting services</h2>
+				</div>
+			</div>
+            <div class='ms-Grid-row'>
+				<div class="ms-Grid-col ms-u-sm12">
+					<div class="tile-container">
+						<div class="tile tile-250 with-link">
+							<a href="/consulting/it-strategy" class="no-style">
+								<div class="tile-image">
+									<i class="ms-Icon ms-Icon--Mail color-green" aria-hidden="true"></i>
+								</div>
+								<div class="tile-caption">
+									<h3 class="ms-font-xl ms-u-textAlignCenter no-margin">IT Strategy</h3>
+								</div>
+							</a>
+						</div>
+
+						<div class="tile tile-250 with-link">
+							<a href="/consulting/innovative-solutions" class="no-style">
+								<div class="tile-image">
+									<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/meeting-iconx96.png" alt="Microsoft OneDrive" />
+								</div>
+								<div class="tile-caption">
+									<h3 class="ms-font-xl ms-u-textAlignCenter no-margin">Innovative Solutions</h3>
+								</div>
+							</a>
+						</div>
+
+						<div class="tile tile-250 with-link">
+							<a href="/consulting/people-and-talent" class="no-style">
+								<div class="tile-image">
+									<img src="https://static2.sharepointonline.com/files/fabric/assets/brand-icons/product/png/onedrive_96x1.png" alt="Microsoft OneDrive" />
+								</div>
+								<div class="tile-caption">
+									<h3 class="ms-font-xl ms-u-textAlignCenter no-margin">People and Talent</h3>
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class='ms-Grid-row ms-u-textAlignCenter'>
+						<div class="tile tile-250 with-link">
+							<a href="/consulting/digital-marketing" class="no-style">
+								<div class="tile-image">
+									<i class="ms-Icon ms-Icon--ChatInviteFriend" aria-hidden="true"></i>
+								</div>
+								<div class="tile-caption">
+									<h3 class="ms-font-xl ms-u-textAlignCenter no-margin">Digital Marketing</h3>
+								</div>
+							</a>
+						</div>
+
+						<div class="tile tile-250 with-link">
+							<a href="/consulting/cloud-computing" class="no-style">
+								<div class="tile-image">
+									<i class="ms-Icon ms-Icon--SocialListeningLogo color-teal" aria-hidden="true"></i>
+								</div>
+								<div class="tile-caption">
+									<h3 class="ms-font-xl ms-u-textAlignCenter no-margin">Cloud Computing</h3>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div class='app-wrapper'>
+          <div class='ms-Grid'>
+            <div class='ms-Grid-row ms-u-textAlignCenter'>
+				<div class="ms-Grid-col ms-u-sm12 ms-u-md6 ms-u-lg4 ms-u-lgPush4 grey-background margins">
+					<div><h3 class="ms-font-xxl">Yes, send me more information</h3></div>
+					<div class="fiber-ninja-form">
+						<?php echo do_shortcode("[ninja_form id=6]"); ?>
+					</div>
+				</div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+
+	<?php get_template_part( 'template-parts/cta-header' ); ?>
+
+    <?php get_footer(); ?>
+  </div>
+</body>
+</html>
