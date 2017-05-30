cd8caac65304c2540c4775d236b7281dda0cac9c
john <gruberjl@gmail.com>
2017-05-30 11:33:40 -0400

    Added 24/7 IT support page

---
+++ page-24-7-it-support.php
@@ -0,0 +1,318 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+  <?php get_template_part( 'template-parts/head/site-head' ); ?>
+</head>
+<body id="body" <?php body_class(); ?>>
+  <div class='site'>
+    <?php get_header(); ?>
+
+    <main class='homepage'>
+      <section class="hero" style='background-image:url("<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/library.jpeg");'>
+        <div class='app-wrapper'>
+          <div class='ms-Grid'>
+            <div class='ms-Grid-row'>
+              <header class="ms-Grid-col ms-u-sm11 ms-u-lg8 ms-u-lgPush1 color-white" style="background: rgba(33, 33, 33, .95);">
+				<div class="ms-u-textAlignCenter">
+					<h1 class="ms-font-su">24/7/365 IT Support</h1>
+					<span class="subtitle ms-font-xxl">Help desk without the wait</span>
+				</div>
+              </header>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="hero">
+        <div class='app-wrapper'>
+          <div class='ms-Grid'>
+            <div class='ms-Grid-row'>
+              <div class="ms-Grid-col ms-u-sm12 ms-u-md6 ms-u-lg4">
+                <div class="card ms-u-textAlignCenter" style="max-width:350px;">
+                  <div><i class="ms-Icon ms-Icon--Heart card-icon color-o365" aria-hidden="true"></i></div>
+                  <div class='card-contents'>
+                    <h2 class="ms-font-su card-title">24/7/365</h2>
+                    <div class='excerpt ms-font-xl'>Our support team is standing by 24/7/365. Available all day, every day. With offices around the United States we're always available, even during natural disasters.</div>
+                  </div>
+                </div>
+              </div>
+              <div class="ms-Grid-col ms-u-sm12 ms-u-md6 ms-u-lg4">
+                <div class="card ms-u-textAlignCenter" style="max-width:350px;">
+                  <div><i class="ms-Icon ms-Icon--Chart card-icon color-green" aria-hidden="true"></i></div>
+                  <div class='card-contents'>
+                    <h2 class="ms-font-su card-title">Any device</h2>
+                    <div class='excerpt ms-font-xl'>Server, PC, Mac, smartphone, tablet, printer, fax, scanner, and cloud services all covered. We have experts on every device and every situation.</div>
+                  </div>
+                </div>
+              </div>
+              <div class="ms-Grid-col ms-u-sm12 ms-u-md6 ms-u-lg4">
+                <div class="card ms-u-textAlignCenter" style="max-width:350px;">
+                  <div><i class="ms-Icon ms-Icon--PieDouble card-icon color-blue" aria-hidden="true"></i></div>
+                  <div class='card-contents'>
+                    <h2 class="ms-font-su card-title">Anywhere Support</h2>
+                    <div class='excerpt ms-font-xl'>Our remote support experts can assist you anywhere in the world, even when you're flying. We'll remote on and get you fixed in no time.</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="hero grey-background">
+        <div class='app-wrapper'>
+          <div class='ms-Grid'>
+            <div class='ms-Grid-row'>
+				<div class="ms-Grid-col ms-u-sm12 ms-u-xl6">
+					<blockquote>
+						<img class="quotemark" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/open-quote.png" />
+						<h3 class="ms-font-xxl quote">I <strong class="color-blue">never wait</strong> for [GitBit IT support] to pick up the phone. When I call, they answer.</h3>
+					</blockquote>
+				</div>
+  				<div class="ms-Grid-col ms-u-sm12 ms-u-xl6">
+					<blockquote>
+						<img class="quotemark" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/open-quote.png" />
+						<h3 class="ms-font-xxl quote">Noninvasive help desk that works around us.</h3>
+					</blockquote>
+				</div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="hero">
+        <div class='app-wrapper'>
+          <div class='ms-Grid'>
+            <div class='ms-Grid-row'>
+				<div class="ms-Grid-col ms-u-sm12 ms-u-xl6 ms-u-textAlignCenter">
+					<div class="ms-Grid-col ms-u-sm12 ms-u-textAlignCenter">
+						<img class="hero-image" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/cindy.jpg" alt="Women smiling" width="30%"/>
+					</div>
+					<div class="ms-Grid-col ms-u-sm12 ms-u-textAlignCenter">
+						<img class="hero-image" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/charles.jpg" alt="Model" width=30%" style="border-radius:50%" />
+						<img class="hero-image" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/dave.jpg" alt="Man on Microsoft Surface" width="30%"/>
+					</div>
+				</div>
+				<div class="ms-Grid-col ms-u-sm12 ms-u-xl6">
+					<div class='ms-Grid-row'>
+						<div class="ms-Grid-col ms-u-sm1 ms-u-textAlignRight">
+							<p class="ms-font-xxl color-green"><i class="ms-Icon ms-Icon--CheckMark" aria-hidden="true"></i></p>
+						</div>
+						<div class="ms-Grid-col ms-u-sm11">
+							<p class="ms-font-xl">Don't overpay. Customizable help desk to fit your needs.</p>
+						</div>
+					</div>
+					<div class='ms-Grid-row'>
+						<div class="ms-Grid-col ms-u-sm1 ms-u-textAlignRight">
+							<p class="ms-font-xxl color-green"><i class="ms-Icon ms-Icon--CheckMark" aria-hidden="true"></i></p>
+						</div>
+						<div class="ms-Grid-col ms-u-sm11">
+							<p class="ms-font-xl">Customizable self-service. We'll setup a company knowledgebase for your team to share processes, workflows, templates, and more. Our experts will continually add our own guides to help you find answers to common problems.</p>
+						</div>
+					</div>
+					<div class='ms-Grid-row'>
+						<div class="ms-Grid-col ms-u-sm1 ms-u-textAlignRight">
+							<p class="ms-font-xxl color-green"><i class="ms-Icon ms-Icon--CheckMark" aria-hidden="true"></i></p>
+						</div>
+						<div class="ms-Grid-col ms-u-sm11">
+							<p class="ms-font-xl">Work with our experts to get the right technology at the best prices. Our team has knowledge in computers, servers, tablets, smart phones, printers, faxes, scanner, software, cloud services, and more.</p>
+						</div>
+					</div>
+					<div class='ms-Grid-row'>
+						<div class="ms-Grid-col ms-u-sm1 ms-u-textAlignRight">
+							<p class="ms-font-xxl color-green"><i class="ms-Icon ms-Icon--CheckMark" aria-hidden="true"></i></p>
+						</div>
+						<div class="ms-Grid-col ms-u-sm11">
+							<p class="ms-font-xl">Anytime, anywhere. Our remote support team can help you anywhere, anytime. No internet? No problem, our experts will walk you through it.</p>
+						</div>
+					</div>
+				</div>
+            </div>
+          </div>
+        </div>
+      </section>
+	  <section class="hero grey-background">
+        <div class='app-wrapper'>
+          <div class='ms-Grid'>
+			<div class="ms-Grid-row">
+				<div class="ms-Grid-col ms-u-sm12 ms-u-textAlignCenter">
+					<h2 class="ms-font-xxl">Coverage</h2>
+				</div>
+			</div>
+            <div class='ms-Grid-row'>
+				<div class="ms-Grid-col ms-u-sm12">
+					<div class="tile-container">
+						<div class="tile tile-250">
+							<div class="tile-image">
+								<i class="ms-Icon ms-Icon--WindowsLogo" aria-hidden="true"></i>
+							</div>
+							<div class="tile-caption">
+								<h3 class="ms-font-xl ms-u-textAlignCenter no-margin">PCs & Laptops</h3>
+							</div>
+						</div>
+						<div class="tile tile-250">
+							<div class="tile-image">
+								<i class="ms-Icon ms-Icon--PC1" aria-hidden="true"></i>
+							</div>
+							<div class="tile-caption">
+								<h3 class="ms-font-xl ms-u-textAlignCenter no-margin">Servers</h3>
+							</div>
+						</div>
+						<div class="tile tile-250">
+							<div class="tile-image">
+								<i class="ms-Icon ms-Icon--TVMonitor" aria-hidden="true"></i>
+							</div>
+							<div class="tile-caption">
+								<h3 class="ms-font-xl ms-u-textAlignCenter no-margin">Macs</h3>
+							</div>
+						</div>
+						<div class="tile tile-250">
+							<div class="tile-image">
+								<i class="ms-Icon ms-Icon--CellPhone" aria-hidden="true"></i>
+							</div>
+							<div class="tile-caption">
+								<h3 class="ms-font-xl ms-u-textAlignCenter no-margin">Smartphones & Tablets</h3>
+							</div>
+						</div>
+						<div class="tile tile-250">
+							<div class="tile-image">
+								<i class="ms-Icon ms-Icon--Print" aria-hidden="true"></i>
+							</div>
+							<div class="tile-caption">
+								<h3 class="ms-font-xl ms-u-textAlignCenter no-margin">Printers, Faxes, & Scanners</h3>
+							</div>
+						</div>
+						<div class="tile tile-250">
+							<div class="tile-image">
+								<i class="ms-Icon ms-Icon--CloudWeather" aria-hidden="true"></i>
+							</div>
+							<div class="tile-caption">
+								<h3 class="ms-font-xl ms-u-textAlignCenter no-margin">Cloud Services</h3>
+							</div>
+						</div>
+						<div class="tile tile-250">
+							<div class="tile-image">
+								<i class="ms-Icon ms-Icon--InternetSharing" aria-hidden="true"></i>
+							</div>
+							<div class="tile-caption">
+								<h3 class="ms-font-xl ms-u-textAlignCenter no-margin">Network Equipment</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="hero" id="benefits-hero">
+        <div class='app-wrapper'>
+          <div class='ms-Grid'>
+			<div class="ms-Grid-row">
+				<div class="ms-Grid-col ms-u-sm12 ms-u-textAlignCenter">
+					<h2 class="ms-font-xxl">Why people choose 24/7 IT Support</h2>
+				</div>
+			</div>
+            <div class='ms-Grid-row'>
+				<div class='ms-Grid-col ms-u-sm12 ms-u-lg4'>
+					<div class='card card-horizontal'>
+						<div class='card-contents'>
+							<h3 class="ms-font-xl ms-u-textAlignCenter no-margin">Antivirus</h3>
+							<div class='excerpt'><p>More intelligent threat detection for all of your computers, servers, and mobile devices. GitBit will keep you secure & monitor your devices using a number of products.</p></div>
+						</div>
+					</div>
+				</div>
+				<div class='ms-Grid-col ms-u-sm12 ms-u-lg4'>
+					<div class='card card-horizontal'>
+						<div class='card-contents'>
+							<h3 class="ms-font-xl ms-u-textAlignCenter no-margin">Junk email</h3>
+							<div class='excerpt'><p>Email is the most attacked place for businesses and enterprises. GitBit's SPAM filtering is designed to protect against phishing, ransomware, and other types of SPAM.</p></div>
+						</div>
+					</div>
+				</div>
+				<div class='ms-Grid-col ms-u-sm12 ms-u-lg4'>
+					<div class='card card-horizontal'>
+						<div class='card-contents'>
+							<h3 class="ms-font-xl ms-u-textAlignCenter no-margin">Cloud app support</h3>
+							<div class='excerpt'><p>GitBit has a variety of partnerships and certifed staff in different cloud application. Our team is specially designed for law firms. We support all cloud solutions including Office 365, TimeSolv, Clio, and more.</p></div>
+						</div>
+					</div>
+				</div>
+				<div class='ms-Grid-col ms-u-sm12 ms-u-lg4'>
+					<div class='card card-horizontal'>
+						<div class='card-contents'>
+							<h3 class="ms-font-xl ms-u-textAlignCenter no-margin">Bring your own device</h3>
+							<div class='excerpt'><p>BYOD is a great way to cut costs and improve productivity. Our team of experts support company owned and employee owned solutions.</p></div>
+						</div>
+					</div>
+				</div>
+				<div class='ms-Grid-col ms-u-sm12 ms-u-lg4'>
+					<div class='card card-horizontal'>
+						<div class='card-contents'>
+							<h3 class="ms-font-xl ms-u-textAlignCenter no-margin">Printers, faxes, and other devices</h3>
+							<div class='excerpt'><p>Our team of experts can support every device. From computers, servers, mobile devices, and IOT. We have expert solutions for all of your needs.</p></div>
+						</div>
+					</div>
+				</div>
+				<div class='ms-Grid-col ms-u-sm12 ms-u-lg4'>
+					<div class='card card-horizontal'>
+						<div class='card-contents'>
+							<h3 class="ms-font-xl ms-u-textAlignCenter no-margin">24/7/365 anytime anywhere</h3>
+							<div class='excerpt'><p>Help desk is staffed with experts 24 hours a day, 7 days a week, 365 days a year. Our team is split across different regions around the United States to support our clients, even during natural disasters.</p></div>
+						</div>
+					</div>
+				</div>
+				<div class='ms-Grid-col ms-u-sm12 ms-u-lg4'>
+					<div class='card card-horizontal'>
+						<div class='card-contents'>
+							<h3 class="ms-font-xl ms-u-textAlignCenter no-margin">Backup & disaster recovery</h3>
+							<div class='excerpt'><p>Secure, offsite backup and disaster recovery. Cloud recovery designed for compliance and to keep your business moving, no matter what happens.</p></div>
+						</div>
+					</div>
+				</div>
+				<div class='ms-Grid-col ms-u-sm12 ms-u-lg4'>
+					<div class='card card-horizontal'>
+						<div class='card-contents'>
+							<h3 class="ms-font-xl ms-u-textAlignCenter no-margin">Ransomware</h3>
+							<div class='excerpt'><p>Ransomware is the fastest growing threat to businesses on the planet. With WannaCry's single attack infecting over 300,000 systems, none of which are GitBit managed.</p></div>
+						</div>
+					</div>
+				</div>
+				<div class='ms-Grid-col ms-u-sm12 ms-u-lg4'>
+					<div class='card card-horizontal'>
+						<div class='card-contents'>
+							<h3 class="ms-font-xl ms-u-textAlignCenter no-margin">Patching</h3>
+							<div class='excerpt'><p>Noninvasive patching and training is provided to all users to keep a business secure and up-to-date in all your technology needs.</p></div>
+						</div>
+					</div>
+				</div>
+				<div class='ms-Grid-col ms-u-sm12 ms-u-lg4'>
+					<div class='card card-horizontal'>
+						<div class='card-contents'>
+							<h3 class="ms-font-xl ms-u-textAlignCenter no-margin">Phone, email, or self service</h3>
+							<div class='excerpt'><p>Support is available by email, phone, or online in our content management system </p></div>
+						</div>
+					</div>
+				</div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="hero">
+        <div class='app-wrapper'>
+          <div class='ms-Grid'>
+            <div class='ms-Grid-row ms-u-textAlignCenter'>
+				<div class="ms-Grid-col ms-u-sm12 ms-u-md6 ms-u-lg4 ms-u-lgPush4 grey-background margins">
+					<div><h3 class="ms-font-xxl">Yes, send me more information</h3></div>
+					<div class="fiber-ninja-form">
+						<?php echo do_shortcode("[ninja_form id=6]"); ?>
+					</div>
+				</div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+
+	<?php get_template_part( 'template-parts/cta-header' ); ?>
+
+    <?php get_footer(); ?>
+  </div>
+</body>
+</html>
